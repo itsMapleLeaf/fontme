5dc6b10eca73fed2110fac9c1e4c45f150ec4fdf
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-27 10:03:32 -0500

    update metadata

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node"
 import {
   Links,
   LiveReload,
@@ -6,13 +6,14 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from "@remix-run/react"
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "fontme",
+  description: "Generate optimized fonts and CSS for self-hosting Google Fonts",
   viewport: "width=device-width,initial-scale=1",
-});
+})
 
 export default function App() {
   return (
@@ -28,5 +29,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
