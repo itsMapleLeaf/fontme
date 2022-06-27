8c2526536bd3a646a48423bc862abb5e67c9712e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-27 10:21:34 -0500

    fetch fonts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,19 @@
+import { useLoaderData } from "@remix-run/react"
+import { googleFontsApiFetch } from "~/modules/gfonts/api.server"
+
+export async function loader() {
+  return googleFontsApiFetch()
+}
+
 export default function Index() {
+  const data: { items: any[] } = useLoaderData()
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.items.map((item) => (
+          <li key={item.family}>{item.family}</li>
+        ))}
       </ul>
     </div>
-  );
+  )
 }
