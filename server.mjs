1999a191c0a0fed8117058753b4262a166f69778
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-29 11:00:44 -0500

    improve http server
    add a logger, compression, and serve static files

--- server.mjs
+++ server.mjs
@@ -1,21 +1,18 @@
-import http from "http"
+// @ts-check
+import { App } from "@tinyhttp/app"
+import compression from "compression"
+import { pinoHttp } from "pino-http"
+import sirv from "sirv"
 import { handler as ssrHandler } from "./dist/server/entry.mjs"
 
-const server = http.createServer((req, res) => {
-  ssrHandler(req, res, (err) => {
-    if (err) {
-      res.writeHead(500)
-      res.end(err.toString())
-    } else {
-      // Serve your static assets here maybe?
-      // 404?
-      res.writeHead(404)
-      res.end()
-    }
-  })
-})
+const port = Number(process.env.PORT || 8080)
 
-const port = process.env.PORT || 8080
-server.listen(port, () => {
-  console.info(`Server started on http://localhost:${port}`)
-})
+new App()
+  .use(pinoHttp())
+  // @ts-expect-error
+  .use(compression())
+  .use(sirv("dist/client"))
+  .use(ssrHandler)
+  .listen(port, () => {
+    console.info(`Server started on http://localhost:${port}`)
+  })
