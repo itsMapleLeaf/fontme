d7b4066e56cf1ad6dcb4931a18ca8b058b5a5e41
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-01 11:58:37 -0500

    highlight css and format with prettier

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,8 @@
-import { DataFunctionArgs } from "@remix-run/node"
+import { DataFunctionArgs, LinksFunction } from "@remix-run/node"
 import { jsonTyped, useLoaderDataTyped } from "remix-typed"
 import { FontSelector } from "~/modules/fonts/font-selector"
-import { generateFontCss } from "~/modules/fonts/generate-font-css"
+import { generateFontCss } from "~/modules/fonts/generate-font-css.server"
+import hljsTheme from "../../node_modules/highlight.js/styles/base16/tomorrow-night.css"
 
 export async function loader({ request }: DataFunctionArgs) {
   const { searchParams } = new URL(request.url)
@@ -11,13 +12,20 @@ export async function loader({ request }: DataFunctionArgs) {
   })
 }
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: hljsTheme },
+]
+
 export default function Index() {
   const { css } = useLoaderDataTyped<typeof loader>()
   return (
     <div className="p-4 grid gap-4">
-      <pre className="bg-base-100 rounded-md p-4 shadow-md whitespace-pre-wrap">
-        {css}
-      </pre>
+      <div className="bg-base-100 rounded-md shadow-md overflow-clip">
+        <pre
+          className="overflow-auto p-4 max-h-[50rem]"
+          dangerouslySetInnerHTML={css}
+        />
+      </div>
     </div>
   )
 }
