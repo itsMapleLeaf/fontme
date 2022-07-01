bc1b87c41df5fd28f9ca1da0eae3465b62c92090
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-01 11:58:37 -0500

    highlight css and format with prettier

--- app/modules/fonts/generate-font-css.server.ts
+++ app/modules/fonts/generate-font-css.server.ts
@@ -1,11 +1,12 @@
-import endent from "endent"
+import hljs from "highlight.js"
+import { format } from "prettier"
 import { FontSelector } from "~/modules/fonts/font-selector"
 import { loadFonts } from "./api.server"
 
 export async function generateFontCss(selector: FontSelector) {
   const fontDict = await loadFonts()
 
-  return Object.entries(selector.selections)
+  let css = Object.entries(selector.selections)
     .flatMap(([familyName, variantNames]) => {
       const variantDict = fontDict.families[familyName]?.variants
 
@@ -19,15 +20,19 @@ export async function generateFontCss(selector: FontSelector) {
       )
     })
     .map(({ family, variant }) => {
-      const css = endent // cue prettier into formatting this as css
-      return css`
+      const normalizedFamily = family.replaceAll(" ", "-")
+      const fontUrl = `/fonts/${normalizedFamily}-${variant.weight}-${variant.style}.woff2`
+      return `
         @font-face {
           font-family: ${JSON.stringify(family)};
           font-style: ${variant.style};
           font-weight: ${variant.weight};
-          src: url(${variant.url}) format("woff2");
+          src: url(${fontUrl}) format("woff2");
         }
       `
     })
     .join("\n\n")
+
+  css = format(css, { parser: "css" })
+  return { __html: hljs.highlight(css, { language: "css" }).value }
 }
