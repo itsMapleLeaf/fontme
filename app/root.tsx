40b7f6a202a6baa274008810e3f979100ad5d0de
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-02 16:45:51 -0500

    redesign

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { DocumentDownloadIcon, TrashIcon } from "@heroicons/react/solid"
 import {
   Deferrable,
   deferred,
@@ -7,20 +8,22 @@ import {
 } from "@remix-run/node"
 import {
   Deferred,
+  Link,
   Links,
   LiveReload,
   Meta,
-  Outlet,
   Scripts,
-  ScrollRestoration,
   useCatch,
   useLoaderData,
   useSearchParams,
 } from "@remix-run/react"
 import type { CatchBoundaryComponent } from "@remix-run/react/routeModules"
+import clsx from "clsx"
 import { FontDict, loadFonts } from "~/modules/fonts/api.server"
 import { FontList, FontListFallback } from "~/modules/fonts/font-list"
 import { SearchForm } from "~/modules/ui/search-form"
+import { FontSelector } from "./modules/fonts/font-selector"
+import { MaxWidthContainer } from "./modules/ui/max-width-container"
 import tailwind from "./tailwind.css"
 
 const searchParamName = "search"
@@ -48,14 +51,18 @@ export const links: LinksFunction = () => [
 
 function Document({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="bg-base-300 text-base-content" data-theme="dark">
+    <html
+      lang="en"
+      className="bg-base-300 text-base-content overflow-y-scroll"
+      data-theme="dark"
+    >
       <head>
         <Meta />
         <Links />
       </head>
       <body>
         {children}
-        <ScrollRestoration />
+        {/* <ScrollRestoration /> */}
         <Scripts />
         <LiveReload />
       </body>
@@ -68,23 +75,58 @@ export default function App() {
 
   const [params] = useSearchParams()
   const searchQuery = params.get(searchParamName) ?? ""
+  const selector = FontSelector.fromParamString(params.get("fonts") ?? "")
+  const selectedVariantCount = Object.values(selector.selections).flat().length
+
+  function getClearSelectedLink() {
+    const newParams = new URLSearchParams(params)
+    newParams.delete("fonts")
+    return `?${newParams.toString()}`
+  }
 
   return (
     <Document>
-      <div className="fixed inset-0 flex">
-        <nav className="bg-base-100 overflow-y-auto shadow-md flex flex-col w-80">
-          <div className="p-4">
-            <SearchForm paramName={searchParamName} />
-          </div>
-          <div className="flex-1">
+      <div className="flex flex-col min-h-screen">
+        <div className="bg-base-200 py-4 shadow-md sticky top-0 z-10">
+          <MaxWidthContainer>
+            <nav className="flex items-center gap-6">
+              <h1 className="text-3xl">fontme</h1>
+              <div className="flex-1">
+                <SearchForm paramName={searchParamName} />
+              </div>
+            </nav>
+          </MaxWidthContainer>
+        </div>
+
+        <main className="py-4">
+          <MaxWidthContainer>
             <Deferred value={fonts} fallback={<FontListFallback />}>
               {(fonts) => <FontList fonts={fonts} searchQuery={searchQuery} />}
             </Deferred>
-          </div>
-        </nav>
-        <main className="flex-1 min-w-0 overflow-y-auto">
-          <Outlet />
+          </MaxWidthContainer>
         </main>
+
+        <div
+          className={clsx(
+            "sticky bottom-0 bg-base-100 py-4 shadow-md transition mt-auto",
+            selectedVariantCount > 0 ? "translate-y-0" : "translate-y-full",
+          )}
+        >
+          <MaxWidthContainer>
+            <footer className="flex items-center gap-4">
+              <aside className="italic opacity-70">
+                {selectedVariantCount} variants selected
+              </aside>
+              <div className="flex-1" />
+              <Link to={getClearSelectedLink()} className="btn gap-2">
+                <TrashIcon className="w-6" /> Clear selected
+              </Link>
+              <button type="button" className="btn gap-2">
+                <DocumentDownloadIcon className="w-6" /> Save fonts
+              </button>
+            </footer>
+          </MaxWidthContainer>
+        </div>
       </div>
     </Document>
   )
