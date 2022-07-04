c4673ecc71d23109982d88aab1fde49dcfd0f324
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-03 23:44:20 -0500

    remove unused code

--- app/modules/fonts/get-default-variant.ts
+++
@@ -1,6 +0,0 @@
-import { FontListItem } from "~/modules/fonts/font-context"
-
-export const getDefaultVariant = (font: FontListItem) =>
-  font.variants.find(
-    (variant) => variant.style === "normal" && variant.weight === "400",
-  ) ?? font.variants[0]!
