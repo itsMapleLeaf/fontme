6438188859382dee60ac5596d145d1c926f7748d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-03 23:37:04 -0500

    jesus christ copilot

--- app/modules/common/omit.ts
+++ app/modules/common/omit.ts
@@ -1,7 +1,7 @@
 export function omit<T, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
   const result: any = {}
   for (const key in obj) {
-    if (keys.includes(key as unknown as K)) {
+    if (!keys.includes(key as unknown as K)) {
       result[key] = obj[key]
     }
   }
